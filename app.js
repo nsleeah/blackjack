7c99fedac74eafb98694333a5340f3f0eae04b76
Leahnicoles <lnsimonn@hotmail.com>
2023-06-17 02:52:23 +0100

    feat(app): track blackjack state

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
 // Declare Variables..
-/*let firstCard = 10
+let firstCard = 10
 let secondCard = 11
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard 
+let hasBlackJack = false
 console.log(sum)
 
 //Logic.
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 👑")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
 }
-*/
 
-// Check if the person is elegible for a birthday card from the King! (100)
+//Cash Out(Keeps Track of State of Game from Above Logic)
+console.log(hasBlackJack) 
 
-let age = 101
-
-if (age < 100) {
-    console.log("Not elegible 😭")
-} else if (age === 100) {
-    console.log("Here is your birthday card from the King! 👑🎂")
-} else {
-    console.log("Not elegible, you have already gotten one 😭" )
-}
-
-
-
-// if less than 100    -> "Not elegible"
-// else if exactly 100 -> "Here is your birthday card from the King!"
-// else                -> "Not elegible, you have already gotten one"
