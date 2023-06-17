f9cbe5d874439c92b1abb25b67ceac616fc3407f
Leahnicoles <lnsimonn@hotmail.com>
2023-06-18 00:47:42 +0100

    complete for loops and arrays exercise

--- app.js
+++ app.js
@@ -65,6 +65,22 @@ for (let i = 10; i < 101; i += 10) {
     console.log(i)
 }
 
+//For Loops and Arrays - Combine Array w/ For Loop
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+    "Great to hear"
+]
+
+for (let i = 0; i < 5; i += 1) {
+    console.log(messages[i])
+}
+
+
+
+
 
 
 
