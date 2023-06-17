6913b4d9ffecfaa6b8a6598133ff1053722e1fb8
Leahnicoles <lnsimonn@hotmail.com>
2023-06-17 18:57:35 +0100

    display message on screen by using text content

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 // Declare Variables..
 let firstCard = 10
-let secondCard = 4
+let secondCard = 30
 let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
 
 
 //Logic.
 const startGame = () => {
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 👑"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
     
-    console.log(message) 
+    messageEl.textContent = message
 }
 
 
