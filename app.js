6da26411442ce8715067a266952716afb9b7bfb9
Leahnicoles <lnsimonn@hotmail.com>
2023-06-17 13:27:35 +0100

    add start game btn, and start game function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Declare Variables..
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
 let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
@@ -8,16 +8,19 @@ let message = ""
 
 
 //Logic.
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 👑"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+const startGame = () => {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 👑"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    
+    console.log(message) 
 }
 
-console.log(message) 
 
 
