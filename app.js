047ce98cf6a01551d054c99bc14b7d1d68e847b8
Leahnicoles <lnsimonn@hotmail.com>
2023-06-17 01:57:20 +0100

    fix logic to make more readable

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ if (sum < 21) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
-} else if (sum > 21) {
+} else {
     console.log("You're out of the game! 😭")
 }
